b40235c201979263907b6226be8aa0af1ba394e8
iMac User <imacuser@iMacs-iMac-7.local>
2020-06-20 21:23:41 +0100

    Updates

---
+++ precache-manifest.22e6019980a7fe4c53016f25874edbca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cafc9e2bb1992db4ca6fc74927a9bbc",
+    "url": "/final-todolist/index.html"
+  },
+  {
+    "revision": "8bb00a7a25125a5e400f",
+    "url": "/final-todolist/static/css/main.5f459fa6.chunk.css"
+  },
+  {
+    "revision": "627942155c2d04a7c397",
+    "url": "/final-todolist/static/js/2.0b4a00fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-todolist/static/js/2.0b4a00fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb00a7a25125a5e400f",
+    "url": "/final-todolist/static/js/main.d4b8d5b4.chunk.js"
+  },
+  {
+    "revision": "5516a00caeb026538661",
+    "url": "/final-todolist/static/js/runtime-main.de134818.js"
+  },
+  {
+    "revision": "9d455a7df70b48a22406e048b1e872a3",
+    "url": "/final-todolist/static/media/page_bkgd.9d455a7d.svg"
+  }
+]);
